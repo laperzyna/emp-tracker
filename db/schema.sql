b31aa51b115e4d056a22369b6a65e480bdc06f3c
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-07-20 17:07:18 -0700

    worked with tutor to add sql tables, and add to index.js file. just need to add data to the tables to be able to display to screen and then update/add data through sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,14 @@
-DROP DATABASE IF EXISTS buisness_db;
-CREATE DATABASE buisness_db;
+DROP DATABASE IF EXISTS business_db;
+CREATE DATABASE business_db;
+
+CREATE TABLE employee(  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    department_name TEXT,
+    department_id int,
+    role_id int,
+    job_title TEXT,
+    role_salary FLOAT,
+    first_name TEXT,
+    last_name TEXT,
+    managers TEXT
+);
--- index.js
+++ index.js
@@ -1,21 +1,81 @@
-const mysql = require('mysql');
+require('dotenv').config();
+const mysql = require('mysql2');
 const inquirer = require("inquirer");
 const table = require("console.table");
+const questions = [{
+    type: "list",
+    message: "Select an Option:",
+    name: "options",
+    choices: [
+        "view all departments",
+        "view all roles",
+        "view all employees",
+        "add a department",
+        "add a role",
+        "add an employee",
+        "update an employee role"
+    ]
+}];
+
+function answerProcess(answers) {
+    const options = answers.options;
+    switch (options) {
+        case "view all departments":
+            connection.query(
+                'SELECT DISTINCT department_name, department_id FROM `employee`',
+                [],
+                function (err, results) {
+                    const tableResults = table.getTable(results);
+                    console.log(tableResults);
+                }
+            );
+            break
+        case "view all roles":
+            break
+        case "view all employees":
+            break
+        case "add a department":
+            break
+        case "add a role":
+            break
+        case "add an employee":
+            break
+        case "update an employee role":
+            break
+    }
+    // console.log(answers);
+}
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: "root",
-    password: "takecare1",
-    database: "buisness_db"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    mainMenu();
+
 });
 
 function mainMenu() {
-    
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            answerProcess(answers);
+            mainMenu();
+        })
+        .catch((error) => {
+            console.error(error);
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else went wrong
+            }
+        });
 }
+
+
+mainMenu();
